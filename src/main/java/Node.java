f618ef81a61c93251cc52ecf83ddf4353f4ca3b1
skydreamer21 <skydreamer210@gmail.com>
2023-04-15 17:20:41 +0900

    feat: Node 클래스 생성

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,29 @@
 public class Node {
     int value, height;
     Node left, right;
+    Node parent;
+    
+    // Node는 생성시 높이 1을 default로 가진다.
+    public Node(int value) {
+        this.value = value;
+        height = 1;
+        parent = null;
+        left = null;
+        right = null;
+    }
+    
+    int getBalanceFactor() {
+        int leftNodeHeight = left == null ? 0 : left.height;
+        int rightNodeHeight = right == null ? 0 : right.height;
+        System.out.printf("%s ----> left : %d, right : %d\n", this, leftNodeHeight, rightNodeHeight);
+        return leftNodeHeight - rightNodeHeight;
+    }
+    
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", height=" + height +
+                '}';
+    }
 }
