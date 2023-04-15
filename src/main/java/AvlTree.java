5756340b961f40cdaf0175aeedf4ec93753111f9
skydreamer21 <skydreamer210@gmail.com>
2023-04-16 08:53:20 +0900

    impl: avl rotate, insert 구현

--- src/main/java/AvlTree.java
+++ src/main/java/AvlTree.java
@@ -1,9 +1,58 @@
+import exceptions.CustomDuplicatedElementException;
+
 public class AvlTree implements iAvlTree{
+    private static final boolean LEFT = true;
+    private static final boolean RIGHT = false;
+    private Node root;
+    private int size;
+    
+    public AvlTree(int rootValue) {
+        root = new Node(rootValue);
+        size = 1;
+    }
+    
+    // Test를 위한 생성자
+    public AvlTree(Node root) {
+        this.root = root;
+    }
     
     @Override
     public void insert(int value) {
-    
+        if (isEmpty()) {
+            root = new Node(value);
+        } else {
+            insertRecur(root, value);
+        }
+        size++;
     }
+    
+    private void insertRecur(Node node, int value) {
+        if (node.value == value) {
+            throw new CustomDuplicatedElementException(String.format("이미 %d는 트리에 있습니다.", value));
+        }
+        
+        if (value < node.value) {
+            if (node.left == null) {
+                node.left = new Node(value);
+                node.left.parent = node;
+                updateHeight(node);
+            } else {
+                insertRecur(node.left, value);
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new Node(value);
+                node.right.parent = node;
+                updateHeight(node);
+            } else {
+                insertRecur(node.right, value);
+            }
+        }
+        // height 갱신
+        // makeBalanced
+    }
+    
+    
     
     @Override
     public boolean contains(int value) {
@@ -38,6 +87,7 @@ public class AvlTree implements iAvlTree{
         }
         
         if (node.parent != null) {
+            System.out.printf("parent 설정\n");
             if (node.parent.left == node) {
                 node.parent.left = nextCenter;
             } else {
@@ -88,18 +138,49 @@ public class AvlTree implements iAvlTree{
     /**
      * 현재 노드의 height 를 두 자식 중 큰 높이 + 1 로 갱신합니다.
      * 만약 갱신이 된다면 부모 또한 갱신합니다.
-     * @param node
      */
     private void updateHeight(Node node) {
+        System.out.printf("<UPDATE> node : %s\n", node);
         if (node == null) {
             return;
         }
         
+        makeBalanced(node);
+        
         int originHeight = node.height;
         node.height = node.findMaxChildHeight() + 1;
+        System.out.printf("<UPDATE> 갱신 후 node : %s\n", node);
         
-        if (originHeight != node.height) {
+        if (node == root) {
+            return;
+        }
+        
+        boolean isUpdatable = (originHeight != node.height)
+                || (node.parent.height != node.parent.findMaxChildHeight() + 1);
+        
+        if (isUpdatable) {
             updateHeight(node.parent);
         }
     }
+    
+    private void makeBalanced(Node node) {
+        if (node.isBalanced()) return;
+        
+        if (node.findBalanceFactor() == 2) { // LL, LR
+            if (node.left.findBalanceFactor() >= 0) { // LL
+                rotateRight(node);
+            } else { // LR
+                rotateLeft(node.left);
+                rotateRight(node);
+            }
+        } else if (node.findBalanceFactor() == -2) { // RR, RL
+            if (node.right.findBalanceFactor() <= 0) { // RR
+                rotateLeft(node);
+            } else { // RL
+                rotateRight(node.right);
+                rotateLeft(node);
+            }
+        }
+    }
+    
 }
