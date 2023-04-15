0de6c9c5ada7e5cd02cf2910f0f59ff8d6edd45b
skydreamer21 <skydreamer210@gmail.com>
2023-04-15 17:55:52 +0900

    test: 자식의 최대 높이 구하는 기능 test 작성

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -33,6 +32,29 @@ class NodeTest {
         root.right = new Node(2);
         root.height = 2;
         assertThat(root.getBalanceFactor()).isEqualTo(-1);
+    }
+    
+    @DisplayName("자식 최대 높이 구하기")
+    @Test
+    void findMaxChildHeightTest1() {
+        root.left = new Node(2);
+        root.right = new Node(5);
+        root.left.height = 5;
+        root.right.height = 2;
+        assertThat(root.findMaxChildHeight()).isEqualTo(5);
+    }
+    
+    @DisplayName("자식이 하나만 있을 때 최대 높이 구하기")
+    @Test
+    void findMaxChildHeightTest2() {
+        root.left = new Node(2);
+        assertThat(root.findMaxChildHeight()).isEqualTo(1);
+    }
+    
+    @DisplayName("자식이 없을 때 최대 높이 구하기")
+    @Test
+    void findMaxChildHeightTest3() {
+        assertThat(root.findMaxChildHeight()).isEqualTo(0);
     }
     
 }
