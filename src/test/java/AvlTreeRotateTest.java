f69ba671a3002d70f0102650cbbde5581419097e
skydreamer21 <skydreamer210@gmail.com>
2023-04-16 13:19:52 +0900

    refactor: 파일명 수정

--- src/test/java/AvlTreeRotateTest.java
+++ src/test/java/AvlTreeRotateTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AvlTreeTest {
+class AvlTreeRotateTest {
     static AvlTree tree;
     
     
