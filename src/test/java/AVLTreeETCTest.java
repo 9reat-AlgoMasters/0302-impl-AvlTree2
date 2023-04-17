9b43430577fe4728def07eba608aa3ed8113f6ec
jhp336 <67370317+jhp336@users.noreply.github.com>
2023-04-18 01:01:40 +0900

    test : 기타 test 작성

---
+++ src/test/java/AVLTreeETCTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AVLTreeETCTest {
+    static AVLTree tree;
+    @BeforeEach
+    void beforeEach() {
+        tree = new AVLTree();
+    }
+
+    @DisplayName("Empty Test")
+    @Test
+    void isEmpty() {
+        assertThat(tree.isEmpty()).isEqualTo(true);
+        tree.insert(1);
+        assertThat(tree.isEmpty()).isEqualTo(false);
+        tree.delete(1);
+        assertThat(tree.isEmpty()).isEqualTo(true);
+    }
+
+    @DisplayName("Contains Test")
+    @Test
+    void contains() {
+        assertThat(tree.contains(15)).isEqualTo(false);
+        tree.insert(15);
+        assertThat(tree.contains(15)).isEqualTo(true);
+        assertThat(tree.contains(1555)).isEqualTo(false);
+        tree.insert(1555);
+        assertThat(tree.contains(1555)).isEqualTo(true);
+
+        tree.delete(15);
+        assertThat(tree.contains(1555)).isEqualTo(true);
+        tree.delete(1555);
+        assertThat(tree.contains(1555)).isEqualTo(false);
+    }
+}
