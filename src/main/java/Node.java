17058e36b0b64b8be8fea682102db2e0698c3b18
LCM <62368629+lecocococo@users.noreply.github.com>
2023-04-18 00:58:41 +0900

    impl: node

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,15 @@
 public class Node {
     int value, height;
     Node left, right;
+
+    public Node(int value){
+        this(value, null, null);
+    }
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.right = right;
+        this.left = left;
+        this.height = 1;
+    }
 }
