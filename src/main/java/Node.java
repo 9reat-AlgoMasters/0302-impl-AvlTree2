04a648bfc67cb9ddb8d28a6a54ec426b9ed394ff
jhp336 <67370317+jhp336@users.noreply.github.com>
2023-04-18 01:05:09 +0900

    impl : Node Class

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,16 @@
 public class Node {
     int value, height;
-    Node left, right;
+    Node leftChild;
+    Node rightChild;
+    Node parent;
+    public Node(int value) {
+        this.value = value;
+        this.height = 1;
+    }
+
+    public Node(int value, Node leftChild, Node rightChild) {
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 }
