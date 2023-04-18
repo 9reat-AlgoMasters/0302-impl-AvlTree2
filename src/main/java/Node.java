b19045661dbb483f20d8f881971f48a4fdcc4128
LCM <62368629+lecocococo@users.noreply.github.com>
2023-04-18 20:46:10 +0900

    fix: node의 height부분 수정

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -2,14 +2,14 @@ public class Node {
     int value, height;
     Node left, right;
 
-    public Node(int value){
-        this(value, null, null);
+    public Node(int value, int height){
+        this(value, null, null, height);
     }
 
-    public Node(int value, Node left, Node right) {
+    public Node(int value, Node left, Node right, int height) {
         this.value = value;
         this.right = right;
         this.left = left;
-        this.height = 1;
+        this.height = height;
     }
 }
