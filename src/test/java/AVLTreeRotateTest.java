f1149658e4715ef2dea7927ace3942aa70ff61a6
jhp336 <67370317+jhp336@users.noreply.github.com>
2023-04-18 16:53:24 +0900

    fix : 변수 명 변경

--- src/test/java/AVLTreeRotateTest.java
+++ src/test/java/AVLTreeRotateTest.java
@@ -18,24 +18,24 @@ class AVLTreeRotateTest {
     void rotateLeft1() {
         Node node1 = new Node(15);
         node1.parent = root;
-        root.leftChild = node1;
+        root.left = node1;
         Node node2 = new Node(10);
         node2.parent = node1;
-        node1.leftChild = node2;
+        node1.left = node2;
         Node node3 = new Node(5);
         node3.parent = node2;
-        node2.leftChild = node3;
+        node2.left = node3;
 
         tree.rotateRight(node1);
 
-        assertThat(node2.leftChild).isEqualTo(node3);
-        assertThat(node2.rightChild).isEqualTo(node1);
+        assertThat(node2.left).isEqualTo(node3);
+        assertThat(node2.right).isEqualTo(node1);
         assertThat(node2.parent).isEqualTo(root);
-        assertThat(node1.leftChild).isEqualTo(null);
-        assertThat(node1.rightChild).isEqualTo(null);
+        assertThat(node1.left).isEqualTo(null);
+        assertThat(node1.right).isEqualTo(null);
         assertThat(node1.parent).isEqualTo(node2);
         assertThat(node1.parent).isEqualTo(node2);
-        assertThat(root.leftChild).isEqualTo(node2);
+        assertThat(root.left).isEqualTo(node2);
     }
 
     @DisplayName("Rotate Test - LR")
@@ -43,25 +43,25 @@ class AVLTreeRotateTest {
     void rotateLeft2() {
         Node node1 = new Node(15);
         node1.parent = root;
-        root.leftChild = node1;
+        root.left = node1;
         Node node2 = new Node(5);
         node2.parent = node1;
-        node1.leftChild = node2;
+        node1.left = node2;
         Node node3 = new Node(10);
         node3.parent = node2;
-        node2.rightChild = node3;
+        node2.right = node3;
 
         tree.rotateLeft(node2);
         tree.rotateRight(node1);
 
-        assertThat(node3.leftChild).isEqualTo(node2);
-        assertThat(node3.rightChild).isEqualTo(node1);
+        assertThat(node3.left).isEqualTo(node2);
+        assertThat(node3.right).isEqualTo(node1);
         assertThat(node3.parent).isEqualTo(root);
-        assertThat(node1.leftChild).isEqualTo(null);
-        assertThat(node1.rightChild).isEqualTo(null);
+        assertThat(node1.left).isEqualTo(null);
+        assertThat(node1.right).isEqualTo(null);
         assertThat(node1.parent).isEqualTo(node3);
         assertThat(node1.parent).isEqualTo(node3);
-        assertThat(root.leftChild).isEqualTo(node3);
+        assertThat(root.left).isEqualTo(node3);
     }
 
     @DisplayName("Rotate Test - RR")
@@ -69,24 +69,24 @@ class AVLTreeRotateTest {
     void rotateRight1() {
         Node node1 = new Node(70);
         node1.parent = root;
-        root.rightChild = node1;
+        root.right = node1;
         Node node2 = new Node(80);
         node2.parent = node1;
-        node1.rightChild = node2;
+        node1.right = node2;
         Node node3 = new Node(90);
         node3.parent = node2;
-        node2.rightChild = node3;
+        node2.right = node3;
 
         tree.rotateLeft(node1);
 
-        assertThat(node2.leftChild).isEqualTo(node1);
-        assertThat(node2.rightChild).isEqualTo(node3);
+        assertThat(node2.left).isEqualTo(node1);
+        assertThat(node2.right).isEqualTo(node3);
         assertThat(node2.parent).isEqualTo(root);
-        assertThat(node1.leftChild).isEqualTo(null);
-        assertThat(node1.rightChild).isEqualTo(null);
+        assertThat(node1.left).isEqualTo(null);
+        assertThat(node1.right).isEqualTo(null);
         assertThat(node1.parent).isEqualTo(node2);
         assertThat(node1.parent).isEqualTo(node2);
-        assertThat(root.rightChild).isEqualTo(node2);
+        assertThat(root.right).isEqualTo(node2);
     }
 
     @DisplayName("Rotate Test - RL")
@@ -94,25 +94,25 @@ class AVLTreeRotateTest {
     void rotateRight2() {
         Node node1 = new Node(70);
         node1.parent = root;
-        root.rightChild = node1;
+        root.right = node1;
         Node node2 = new Node(90);
         node2.parent = node1;
-        node1.rightChild = node2;
+        node1.right = node2;
         Node node3 = new Node(80);
         node3.parent = node2;
-        node2.leftChild = node3;
+        node2.left = node3;
 
         tree.rotateRight(node2);
         tree.rotateLeft(node1);
 
-        assertThat(node3.leftChild).isEqualTo(node1);
-        assertThat(node3.rightChild).isEqualTo(node2);
+        assertThat(node3.left).isEqualTo(node1);
+        assertThat(node3.right).isEqualTo(node2);
         assertThat(node3.parent).isEqualTo(root);
-        assertThat(node1.leftChild).isEqualTo(null);
-        assertThat(node1.rightChild).isEqualTo(null);
+        assertThat(node1.left).isEqualTo(null);
+        assertThat(node1.right).isEqualTo(null);
         assertThat(node1.parent).isEqualTo(node3);
         assertThat(node1.parent).isEqualTo(node3);
-        assertThat(root.rightChild).isEqualTo(node3);
+        assertThat(root.right).isEqualTo(node3);
     }
 
 }
